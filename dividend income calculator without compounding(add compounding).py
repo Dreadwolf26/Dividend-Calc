c32628504848f1c0018fcd2e9a704b1c5249c966
Dreadwolf26 <107774155+Dreadwolf26@users.noreply.github.com>
2022-10-19 08:55:50 -0400

    Update dividend income calculator without compounding(add compounding).py
    Notes added for readability

--- dividend income calculator without compounding(add compounding).py
+++ dividend income calculator without compounding(add compounding).py
@@ -5,10 +5,11 @@ Created on Fri Sep  9 08:09:10 2022
 @author: chris
 """
 
+
 def dividend(a):
     stock_name = "SPY" #stock name
     investment_amount = a #investment amount in dollars 
-    dividend_rate = 1.50 #yearly dividend rate 
+    dividend_rate = 1.50 #yearly dividend rate in dollars
     market_price = 400.38 #current markeyprice of stock 
     return print (stock_name, investment_amount * dividend_rate / market_price )
 dividend(5000) #74.9288176232579
